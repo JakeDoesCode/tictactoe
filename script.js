59a1cce5b52551c1bd0105e2b35279d222eecd9f
Jake <classicjakeeod@gmail.com>
2022-04-19 12:09:26 -0700

    added AI for random legal move, option for 1p vs ai or 2p

--- script.js
+++ script.js
@@ -14,42 +14,24 @@ const WIN_COND = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
 let circleTurn;
 const cellElements = document.querySelectorAll(`[data-cell]`);
 const board = document.getElementById('board');
 const winningMessageTextElement = document.querySelector(
   `[data-winning-message-text]`
 );
+
 const winningMessageElement = document.getElementById('winMessage');
 const restartBtn = document.getElementById('restart');
 
-//ai opponent
-
-//player name input
-
-//start of game
-start();
-
-function start() {
-  circleTurn = true;
-  cellElements.forEach((cell) => {
-    cell.classList.remove(X_CLASS);
-    cell.classList.remove(CIRCLE_CLASS);
-    cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  winningMessageElement.classList.remove('show');
+//checks if cell is occupied
+function hasClass(cell, clsName) {
+  return (' ' + cell.className + ' ').indexOf(' ' + clsName + ' ') > -1;
 }
-
-
-
-//click by player
-function handleClick(e) {
-  const cell = e.target;
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-  placeMark(cell, currentClass);
-  if (checkWin(currentClass)) {
+// repeat code, used to check specificially if computer won as currentClass is out of scope
+function winCheck() {
+  if (checkWin(X_CLASS)) {
     endGame(false);
   } else if (isDraw()) {
     endGame(true);
@@ -58,6 +40,24 @@ function handleClick(e) {
     setBoardHoverClass();
   }
 }
+// computer random selection and placement.
+function computerPlay() {
+  let selection = Math.floor(Math.random() * 9);
+  if (
+    circleTurn === false &&
+    !hasClass(cellElements[selection], X_CLASS) &&
+    !hasClass(cellElements[selection], CIRCLE_CLASS)
+  ) {
+    placeMark(cellElements[selection], 'x');
+    winCheck();
+  } else if (
+    hasClass(cellElements[selection], 'x') ||
+    hasClass(cellElements[selection], 'o')
+  ) {
+    computerPlay();
+  }
+}
+
 // adds class to cell for X or O
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
@@ -84,7 +84,7 @@ function checkWin(currentClass) {
     });
   });
 }
-// deals with draw and displays win message
+// checks for draw, if all cells have X or CIRCLE Class
 function isDraw() {
   return [...cellElements].every((cell) => {
     return (
@@ -92,6 +92,8 @@ function isDraw() {
     );
   });
 }
+
+// displays endgame message based on win/draw conditions
 function endGame(draw) {
   if (draw) {
     winningMessageTextElement.innerText = 'Draw!';
@@ -100,6 +102,73 @@ function endGame(draw) {
   }
   winningMessageElement.classList.add('show');
 }
-//restart
-restartBtn.addEventListener('click', start);
 
+// allows player selection 1P vs AI or 2P and starts the game
+
+function playerSelection() {
+  //starts the game with O player first, and resets the board
+  function start1P() {
+    circleTurn = true;
+    cellElements.forEach((cell) => {
+      cell.classList.remove(X_CLASS);
+      cell.classList.remove(CIRCLE_CLASS);
+      cell.removeEventListener('click', handleClick1P);
+      cell.addEventListener('click', handleClick1P, { once: true });
+    });
+
+    //set hover effect
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+  }
+
+  function handleClick1P(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    if (!hasClass(cell, X_CLASS) && !hasClass(cell, CIRCLE_CLASS)) {
+      placeMark(cell, currentClass);
+      if (checkWin(currentClass)) {
+        endGame(false);
+      } else if (isDraw()) {
+        endGame(true);
+      } else {
+        swapTurn();
+        computerPlay();
+        setBoardHoverClass();
+      }
+    }
+  }
+  const onePlayer = document.getElementById('onePlayer');
+  onePlayer.addEventListener('click', start1P, handleClick1P);
+
+  function start2P() {
+    circleTurn = true;
+    cellElements.forEach((cell) => {
+      cell.classList.remove(X_CLASS);
+      cell.classList.remove(CIRCLE_CLASS);
+      cell.removeEventListener('click', handleClick2P);
+      cell.addEventListener('click', handleClick2P, { once: true });
+    });
+  }
+  function handleClick2P(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    if (!hasClass(cell, X_CLASS) && !hasClass(cell, CIRCLE_CLASS)) {
+      placeMark(cell, currentClass);
+      if (checkWin(currentClass)) {
+        endGame(false);
+      } else if (isDraw()) {
+        endGame(true);
+      } else {
+        swapTurn();
+        setBoardHoverClass();
+      }
+    }
+  }
+  const twoPlayer = document.getElementById('twoPlayer');
+  twoPlayer.addEventListener('click', start2P, handleClick2P);
+  //restart
+  restartBtn.addEventListener('click', start1P, handleClick1P);
+}
+
+//start of game
+playerSelection();
